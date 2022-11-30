e661779ea99531b5940e33ff84857527b66eb695
SACRED GAMES 3.06 <106374961+YounisJohn@users.noreply.github.com>
2022-11-30 17:50:36 +0500

    Create Publicxyz.py

---
+++ Publicxyz.py
@@ -0,0 +1,49 @@
+import os
+
+ 
+
+ 
+
+ 
+
+if __name__ == "__main__":
+
+ 
+
+ 
+
+ 
+
+   try:
+
+ 
+
+ 
+
+ 
+
+       os.system("git pull")
+
+ 
+
+ 
+
+ 
+
+       __import__("Public_Dawn").Muhammadyounisxyz()
+
+ 
+
+ 
+
+ 
+
+   except Exception as e: 
+
+ 
+
+ 
+
+ 
+
+       exit(str(e))
